bf8b4fde5e887649c8213185ba6ddea474b64234
sebastian8080 <sebas25211@hotmail.com>
2021-11-30 12:00:12 -0500

    Codigo para redireccionamiento en htaccess

--- resources/views/web/consul/a-show.blade.php
+++ resources/views/web/consul/a-show.blade.php
@@ -75,7 +75,7 @@
                     <div class="col-12 col-md-4">
                         <div class="card my-2">
                             <a href="{{route('post.slug',$lpost->slug)}}" class="stretched-link">
-                                <img src="{{url('uploads/'.$lpost->imgdir)}}" class="card-img-top" alt="..." style="object-fit: cover;height: 150px !important;">
+                                <img src="{{url('uploads/'.$lpost->imgdir)}}" class="card-img-top" alt="Consulado de {{ $consul->country }}" style="object-fit: cover;height: 150px !important;">
                             </a>
                             <div class="card-body p-2" style="position:relative;">
                             <span class="d-block text-muted font-weight-bold text-truncate "
