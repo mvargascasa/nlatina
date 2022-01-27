7460dce52a239e8c8fba625f9606f0bedee4561a
sebastian8080 <sebas25211@hotmail.com>
2022-01-27 14:25:52 -0500

    Probando regreso de pagina

--- resources/views/web/partner.blade.php
+++ resources/views/web/partner.blade.php
@@ -1,5 +1,10 @@
 @extends('layouts.web')
 @section('header')
+@php
+    header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
+    header("Cache-Control: post-check=0, pre-check=0", false);
+    header("Pragma: no-cache");
+@endphp
     <title>Partner de Notaria Latina - {{ $partner->name }} {{$partner->lastname}}</title>
     <style>
         .bg-header{
