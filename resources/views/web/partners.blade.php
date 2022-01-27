04240310c8526767792e0374d71b1d01bca1373f
sebastian8080 <sebas25211@hotmail.com>
2022-01-27 12:29:52 -0500

    Quitando especialidad

--- resources/views/web/partners.blade.php
+++ resources/views/web/partners.blade.php
@@ -312,7 +312,7 @@
                                         {{ $partner->name }} {{ $partner->lastname }}
                                     </b>
                                 </h5> 
-                                <p>{{ $partner->specialty }}</p>
+                                {{-- <p>{{ $partner->specialty }}</p> --}}
                                  @foreach ($partner->specialties as $specialty)
                                 <div class="d-inline" style="font-size: 14px">
                                     • {{ $specialty->name_specialty }}
