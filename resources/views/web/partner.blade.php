80393c07725e4095266847db7dbdb55678ae8382
sebastian8080 <sebas25211@hotmail.com>
2022-02-03 15:14:50 -0500

    Cambiando propiedad og type de meta tag

--- resources/views/web/partner.blade.php
+++ resources/views/web/partner.blade.php
@@ -4,7 +4,7 @@
     <meta property="og:site_name" content="https://notarialatina.com"/>
     <meta property="og:url" content="{{ Request::url() }}"/>
     <meta property="og:description" content="{{ $partner->specialty}}"/>
-    <meta property="og:type" content="website"/>
+    <meta property="og:type" content="article"/>
     <meta property="og:locale" content="es"/>
     <meta property="og:image" content="https://notarialatina.com/storage/{{$partner->img_profile}}"/>
     <meta property="og:image:width" content="400" />
