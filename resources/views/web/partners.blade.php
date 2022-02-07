d2094db2d2bfb1ef12abfae3e907441105f83032
sebastian8080 <sebas25211@hotmail.com>
2022-02-07 12:12:31 -0500

    Aumentando padding right de partners

--- resources/views/web/partners.blade.php
+++ resources/views/web/partners.blade.php
@@ -146,7 +146,7 @@
         @media screen and (max-width: 580px){
             .contenido{
                 margin-left: 0% !important;
-                margin-right: -10% !important;
+                margin-right: -12% !important;
             }
             #rowDataPartner{
                margin-left: -25% !important;
