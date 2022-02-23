2200400a32bdee1bcc6de5b3c4419d1e7ec72e64
sebastian8080 <sebas25211@hotmail.com>
2022-02-23 15:49:56 -0500

    Probando con semilla name en randomOrder

--- app/Http/Controllers/WebController.php
+++ app/Http/Controllers/WebController.php
@@ -136,23 +136,21 @@ class WebController extends Controller
         $countries = Country::select(['id', 'name_country'])->orderBy('name_country', 'asc')->get();
         $states = State::where('country_id', $request->country)->get();
         $partners = Partner::select(['id', 'img_profile', 'name', 'lastname', 'title', 'state', 'codigo_pais', 'specialty', 'country_residence', 'phone', 'email', 'slug'])
-                ->inRandomOrder('id')
-                ->whereNotIn('id', $request->session()->get('partners', []))
+                ->inRandomOrder('name')
                 ->where('status', 'PUBLICADO')
                 // ->orderBy('id', 'DESC')
                 ->country($request->country)
                 ->state($request->state)
                 ->specialties($request->specialty)
                 // ->limit($dataToLoad)
-                ->paginate(16)
-                ->onEachSide(1);
+                ->paginate(16);
                 // ->inRandomOrder()
                 // ->get();
-        if($request->has('page')){
-            $request->session()->push('photos', $partners->pluck('id'));
-        } else {
-            $request->session()->forget('partners');
-        }
+        // if($request->has('page')){
+        //     $request->session()->push('photos', $partners->pluck('id'));
+        // } else {
+        //     $request->session()->forget('partners');
+        // }
 
         $partnersCount = Partner::where('status', 'PUBLICADO')
                     ->orderBy('id', 'DESC')
