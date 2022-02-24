12c81b58a70c094a710a0061338e0c38e3d54e48
sebastian8080 <sebas25211@hotmail.com>
2022-02-24 16:31:27 -0500

    Cambiando paginacion a 16

--- app/Http/Controllers/WebController.php
+++ app/Http/Controllers/WebController.php
@@ -144,7 +144,7 @@ class WebController extends Controller
                 ->inRandomOrder('name')
                 // ->orderBy('id', 'DESC')
                 // ->limit($dataToLoad)
-                ->paginate(1);
+                ->paginate(16);
                 // ->inRandomOrder()
                 // ->get();
 
@@ -186,7 +186,7 @@ class WebController extends Controller
                 ->inRandomOrder('name')
                 // ->orderBy('id', 'DESC')
                 // ->limit($dataToLoad)
-                ->paginate(1);
+                ->paginate(16);
                 // ->inRandomOrder()
                 // ->get();
 
