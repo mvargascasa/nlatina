63dea3b18328f37d3378211cb14d3fa50e233220
sebastian8080 <sebas25211@hotmail.com>
2022-03-02 17:34:08 -0500

    Retirando el console.log del archivo Country.js

--- public/js/Country.js
+++ public/js/Country.js
@@ -4,11 +4,9 @@ class Country {
 
     static setCountryId(id){
         this.country_id = id;
-        console.log('ID SETEADO ' + this.country_id);
     }
     
     static getCountryId(){
-        console.log('GET ID ' + this.country_id);
         return this.country_id;
     }
 }
