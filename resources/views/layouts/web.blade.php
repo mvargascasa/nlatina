1286eed280f2adb320501fd0070a3314df155da8
sebastian8080 <sebas25211@hotmail.com>
2022-03-03 17:21:34 -0500

    Retirando la carga asincrona de los css

--- resources/views/layouts/web.blade.php
+++ resources/views/layouts/web.blade.php
@@ -5,9 +5,9 @@
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   <link rel="icon" href="{{asset('img/favicon.ico')}}" type="image/x-icon" />
   {{-- <link href="https://fonts.googleapis.com/css2?family=Antic+Didone&display=swap" rel="stylesheet"> --}}
-  <link rel="stylesheet" href="{{asset('css/bootstrap.min.css')}}" media="print" onload="this.media='all'">
-  <link rel="stylesheet" href="{{asset('css/styles.min.css')}}" media="print" onload="this.media='all'">
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.14.0/css/all.min.css" media="print" onload="this.media='all'"/>
+  <link rel="stylesheet" href="{{asset('css/bootstrap.min.css')}}">
+  <link rel="stylesheet" href="{{asset('css/styles.min.css')}}">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.14.0/css/all.min.css"/>
   <meta name="facebook-domain-verification" content="lz9luqstj366xp6jboc5k6mt4m4ssm" />
 
 <?php
