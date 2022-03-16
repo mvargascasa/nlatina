eea26b26d90437fd08e040544d748d5b1c71cebf
sebastian8080 <sebas25211@hotmail.com>
2022-03-16 12:24:33 -0500

    Agregando alert informandole de la nueva tab clientes

--- resources/views/admin/partner/customersview.blade.php
+++ resources/views/admin/partner/customersview.blade.php
@@ -12,6 +12,19 @@
 
 @section('content')
     <div class="container">
+        <p id="txtpartner" style="display: none">{{ Str::lower(Auth::guard('partner')->user()->name.Str::lower(Auth::guard('partner')->user()->lastname).Str::lower(Auth::guard('partner')->user()->id)) }}</p>
+        {{-- ALERT PARA INFORMAR DE LA NUEVA PAGINA DE MIS CLIENTES --}}
+        <div id="alertinfoclientes" class="alert alert-success alert-dismissible fade show mt-3" role="alert">
+            <div>
+                <h4 class="alert-heading">Bienvenido/a {{ Auth::guard('partner')->user()->name }}!</h4>
+                <button type="button" class="close" data-dismiss="alert" aria-label="Close" onclick="setLocalStorage('{{ Str::lower(Auth::guard('partner')->user()->name.Str::lower(Auth::guard('partner')->user()->lastname).Str::lower(Auth::guard('partner')->user()->id)) }}');">
+                <span aria-hidden="true">&times;</span>
+            </div>
+            <p>Queremos informarle que hemos creado esta sección para guardar un registro de sus clientes. Una vez que alguien contacte por usted, aparecerá en este listado.</p>
+            <hr>
+            <p class="mb-0" style="font-weight: bold">Notaria Latina le desea lo mejor! 👨‍⚖️</p>
+        </div>
+
         <h4 class="mt-3 mb-3 text-center" style="font-weight: bold">Listado de clientes</h4>
         @if (count($customers) > 0)
             <div class="row">
@@ -77,5 +90,21 @@
 @endsection
 
 @section('end-scripts')
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script>
+        document.addEventListener("DOMContentLoaded", function(event) {
+            var txtpartner = document.getElementById('txtpartner');
+            if (localStorage.getItem(txtpartner.textContent)) {
+                $('#alertinfoclientes').hide();
+            } else {
+                $('#alertinfoclientes').show();
+            }
+        });
 
+        function setLocalStorage(partner){
+            if(!localStorage.getItem(partner)){
+                localStorage.setItem(partner, true);
+            }
+        }
+    </script>
 @endsection
