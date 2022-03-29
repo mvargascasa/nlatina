de6b3c16e9fda5c44c676e34715de7cb9b107afb
sebastian8080 <sebas25211@hotmail.com>
2022-03-29 12:13:28 -0500

    Retirando email del customer partner momentaneo

--- resources/views/admin/partner/show.blade.php
+++ resources/views/admin/partner/show.blade.php
@@ -420,6 +420,7 @@
                                 <thead>
                                 <tr>
                                     <th scope="col">Nombre</th>
+                                    <th scope="col">Correo</th>
                                     <th scope="col">País</th>
                                     <th scope="col">Teléfono</th>
                                     <th scope="col">Mensaje</th>
@@ -430,10 +431,11 @@
                                     @foreach($partner->customers as $customer)
                                         <tr>
                                             <th>{{ $customer->nombre }}</th>
+                                            <td><a href="mailto:{{$customer->email}}">{{ $customer->email }}</a></td>
                                             <td>{{ $customer->pais }}</td>
                                             <td>{{ $customer->telefono }}</td>
                                             <td>{{ $customer->mensaje }}</td>
-                                            <td>{{ Str::limit($$customer->pivot->created_at, 10, '') }}</td>
+                                            <td>{{ $customer->pivot->created_at }}</td>
                                         </tr>      
                                     @endforeach
                                 </tbody>
