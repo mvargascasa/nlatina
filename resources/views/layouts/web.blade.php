d8800459d640aed01884975cf4c8ca1b2eb92947
sebastian8080 <sebas25211@hotmail.com>
2022-04-27 10:16:07 -0500

    Retirando script de facebook plugin en la ruta /partners

--- resources/views/layouts/web.blade.php
+++ resources/views/layouts/web.blade.php
@@ -334,7 +334,7 @@ $consuls = \App\Consulate::select('country', 'slug')->orderBy('country')->get();
     var path = window.location.pathname;
 
     //CARGO LOS RECURSOS SI LAS URLS SON DIFERENTES AL DE LAS OFICINAS
-    if(!(path.match('/newjersey') || path.match('/newyork') || path.match('/florida'))){
+    if(!(path.match('/newjersey') || path.match('/newyork') || path.match('/florida') || path.match('/partners'))){
         document.getElementsByTagName("script")[0].parentNode.appendChild(scriptFacebookPlugin);
     } else {
         if(screen.width > 580){
