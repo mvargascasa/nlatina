3e1c61d24f8732be3c6af5ab08932e8b44aaf9e6
sebastian8080 <sebas25211@hotmail.com>
2022-05-06 15:40:11 -0500

    Cambiando formato de imagenes jpg a webp en las landing
    Agregando meta description en la pagina login partners y apostilla-express-24-horas

--- resources/views/admin/partner/login.blade.php
+++ resources/views/admin/partner/login.blade.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Login Partners</title>
+    <meta name="description" content="Inicio de Sesión - Partners de Notaria Latina ⚖">
     <link rel="stylesheet" href="{{asset('css/bootstrap.min.css')}}">
     <link rel="stylesheet" href="{{asset('css/styles.min.css')}}">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.14.0/css/all.min.css"/>
