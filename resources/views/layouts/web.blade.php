5b46fb88af97a15ace54e1d0bfcf52495fa27542
sebastian8080 <sebas25211@hotmail.com>
2022-05-10 15:32:39 -0500

    Agregando defer en el script de google analytics

--- resources/views/layouts/web.blade.php
+++ resources/views/layouts/web.blade.php
@@ -15,7 +15,7 @@ $actual_link = "http://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
 if(strpos($actual_link, 'localhost') === false){
 ?>
   <!-- Global site tag (gtag.js) - Google Analytics -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=UA-124437679-3"></script>
+<script defer src="https://www.googletagmanager.com/gtag/js?id=UA-124437679-3"></script>
 <script>
   window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
