8a31d5c557c4a58aaf197c9a455574a121cd7474
sebastian8080 <sebas25211@hotmail.com>
2022-05-10 12:24:47 -0500

    Clase lazy en imagenes

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -91,7 +91,7 @@
     </ol>
     <div class="carousel-inner">
         <div class="carousel-item active"  style="background:rgba(2, 2, 2, 0.5);">
-          <img src="{{asset('img/5.webp')}}" class="d-block w-100" alt="..." 
+          <img data-src="{{asset('img/5.webp')}}" class="lazy d-block w-100" alt="..." 
           style="height: 550px;object-fit: cover; object-position: left top;">
           <div class="carousel-caption">
               <h1 class="tit-not">Notaría Pública</h1>
@@ -111,7 +111,7 @@
 
 {{-- @if(!$mobile) --}}
         <div class="carousel-item"  style="background:rgba(2, 2, 2, 0.5);">
-          <img src="{{asset('img/2.jpg')}}" class="d-block w-100" alt="..." 
+          <img data-src="{{asset('img/2.jpg')}}" class="lazy d-block w-100" alt="..." 
           style="height: 550px;object-fit: cover; object-position: left bottom;">
           <div class="carousel-caption">
               <h1 id="titleTraducciones" class="tit-not" style="margin-bottom: 6%">TRADUCCIONES</h1>
@@ -141,7 +141,7 @@
           </div>
         </div>
         <div class="carousel-item"  style="background:rgba(2, 2, 2, 0.5);">
-          <img src="{{asset('img/4.jpg')}}" class="d-block w-100" alt="..." 
+          <img data-src="{{asset('img/4.jpg')}}" class="lazy d-block w-100" alt="..." 
           style="height: 550px;object-fit: cover; object-position: left bottom;">
           <div class="carousel-caption">
               <h1 class="tit-not">PODERES GENERALES</h1>
@@ -279,6 +279,7 @@
         document.getElementById('imgdoc').src = "img/docverify-approved-enotary-small.png";
         document.getElementById('imgnna').src = "img/national-notary-association.png";
     });
+  document.addEventListener("DOMContentLoaded",function(){var e;if("IntersectionObserver"in window){e=document.querySelectorAll(".lazy");var n=new IntersectionObserver(function(e,t){e.forEach(function(e){if(e.isIntersecting){var t=e.target;t.src=t.dataset.src,t.classList.remove("lazy"),n.unobserve(t)}})});e.forEach(function(e){n.observe(e)})}else{var t;function r(){t&&clearTimeout(t),t=setTimeout(function(){var n=window.pageYOffset;e.forEach(function(e){e.offsetTop<window.innerHeight+n&&(e.src=e.dataset.src,e.classList.remove("lazy"))}),0==e.length&&(document.removeEventListener("scroll",r),window.removeEventListener("resize",r),window.removeEventListener("orientationChange",r))},20)}e=document.querySelectorAll(".lazy"),document.addEventListener("scroll",r),window.addEventListener("resize",r),window.addEventListener("orientationChange",r)}});
   </script>
 @endsection
 
