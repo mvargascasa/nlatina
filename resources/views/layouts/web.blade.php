6200d4b09c8a870804ca00266bcd9eab61ccc652
sebastian8080 <sebas25211@hotmail.com>
2022-05-12 11:08:48 -0500

    Cambiando forma de cargar el css de bootstrap con preload

--- resources/views/layouts/web.blade.php
+++ resources/views/layouts/web.blade.php
@@ -14,7 +14,7 @@
 
     <link rel="icon" href="{{asset('img/favicon.ico')}}" type="image/x-icon" />
     {{-- <link href="https://fonts.googleapis.com/css2?family=Antic+Didone&display=swap" rel="stylesheet"> --}}
-    <link rel="stylesheet" href="{{asset('css/bootstrap.min.css')}}">
+    <link rel="preload" href="{{asset('css/bootstrap.min.css')}}" as="style" onload="this.rel='stylesheet'">
     <link rel="preload" href="{{asset('css/styles.min.css')}}" as="style" onload="this.rel='stylesheet'">
     {{-- <link async href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.14.0/css/all.min.css" rel="preload" as="style" onload="this.rel='stylesheet'"/>
     <noscript><link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.14.0/css/all.min.css" rel="preload" as="style" onload="this.rel='stylesheet'"></noscript> --}}
