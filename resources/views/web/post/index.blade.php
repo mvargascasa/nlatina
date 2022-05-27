ec181e0d63408ca6e0cad4ddfd5e5a6184a2daa9
sebastian8080 <sebas25211@hotmail.com>
2022-05-27 17:36:46 -0500

    Agregando clase lazy para la carga de imagenes que no se ven en pantalla

--- resources/views/web/post/index.blade.php
+++ resources/views/web/post/index.blade.php
@@ -43,7 +43,7 @@
                 <div class="col-12 col-md-4">
                     <div class="card my-2">
                         <a href="{{route('post.slug',$post->slug)}}" class="stretched-link">
-                            <img src="{{url('uploads/'.$post->imgdir)}}" class="card-img-top" alt="Imagen {{ $post->name }}" style="object-fit: cover;height: 150px !important;">
+                            <img data-src="{{url('uploads/'.$post->imgdir)}}" class="lazy card-img-top" alt="Imagen {{ $post->name }}" style="object-fit: cover;height: 150px !important;">
                         </a>
                         <div class="card-body p-2" style="position:relative;">
                         <span class="d-block text-muted font-weight-bold text-truncate "
@@ -72,6 +72,7 @@
     window.addEventListener('load', (event) => {
         document.getElementById('prisection').style.backgroundImage = "url('img/blog-notaria-latina-2021.png')";
     });
+    document.addEventListener("DOMContentLoaded",function(){var e;if("IntersectionObserver"in window){e=document.querySelectorAll(".lazy");var n=new IntersectionObserver(function(e,t){e.forEach(function(e){if(e.isIntersecting){var t=e.target;t.src=t.dataset.src,t.classList.remove("lazy"),n.unobserve(t)}})});e.forEach(function(e){n.observe(e)})}else{var t;function r(){t&&clearTimeout(t),t=setTimeout(function(){var n=window.pageYOffset;e.forEach(function(e){e.offsetTop<window.innerHeight+n&&(e.src=e.dataset.src,e.classList.remove("lazy"))}),0==e.length&&(document.removeEventListener("scroll",r),window.removeEventListener("resize",r),window.removeEventListener("orientationChange",r))},20)}e=document.querySelectorAll(".lazy"),document.addEventListener("scroll",r),window.addEventListener("resize",r),window.addEventListener("orientationChange",r)}});
   </script>
 @endsection
 
