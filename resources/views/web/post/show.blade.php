44747d4efbd59e8a9909409e6b9423a1e766a829
sebastian8080 <sebas25211@hotmail.com>
2022-05-31 15:16:05 -0500

    Disminuyendo tamaño de letra de los h2 en los post del bolg

--- resources/views/web/post/show.blade.php
+++ resources/views/web/post/show.blade.php
@@ -27,7 +27,7 @@
             h1{
                  font-size: 25px !important; 
             }
-            h2{font-size: 20px !important}
+            h2{font-size: 20px !important; font-weight: 500 !important}
         }
     </style>
 @endsection
