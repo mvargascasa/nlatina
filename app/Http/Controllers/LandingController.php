d22063d9008c4d287773344df737ecfa9ad52a6e
sebastian8080 <sebas25211@hotmail.com>
2022-06-06 15:22:02 -0500

    Descomentando funcion de envio de mail a notarialatina

--- app/Http/Controllers/LandingController.php
+++ app/Http/Controllers/LandingController.php
@@ -65,7 +65,7 @@ class LandingController extends Controller
             $header .= 'From: <lead_landing@notarialatina.com>' . "\r\n";
             $header .= "MIME-Version: 1.0\r\n";
             $header .= "Content-type:text/html;charset=UTF-8" . "\r\n";
-            //mail('notariapublicalatina@gmail.com'.$sendoffices,'Lead Landing: '.strip_tags($request->aaa), $message, $header);  
+            mail('notariapublicalatina@gmail.com'.$sendoffices,'Lead Landing: '.strip_tags($request->aaa), $message, $header);  
             mail('sebas31051999@gmail.com','Lead Landing: '.strip_tags($request->aaa), $message, $header);  
         }
 
@@ -83,7 +83,7 @@ class LandingController extends Controller
             $header .= 'From: <lead_landing@notarialatina.com>' . "\r\n";
             $header .= "MIME-Version: 1.0\r\n";
             $header .= "Content-type:text/html;charset=UTF-8" . "\r\n";
-            //mail('notariapublicalatina@gmail.com'.$sendoffices,'Lead Landing: '.strip_tags($request->fname), $message, $header);
+            mail('notariapublicalatina@gmail.com'.$sendoffices,'Lead Landing: '.strip_tags($request->fname), $message, $header);
             mail('sebas31051999@gmail.com','Lead Landing: '.strip_tags($request->aaa), $message, $header);      
         }
 
