85fe7d690df62be09f8f4d205db562642c6ad4cb
sebastian8080 <sebas25211@hotmail.com>
2022-06-17 09:44:17 -0500

    Agregando robots noindex en la pagina de thank despues de enviar un lead

--- resources/views/landing/thank.blade.php
+++ resources/views/landing/thank.blade.php
@@ -7,6 +7,7 @@
   <link rel="stylesheet" href="{{asset('css/bootstrap.min.css')}}">
   <title>Apostillas Inmediatas en Estados Unidos - Notaria Latina</title>
   <!-- Global site tag (gtag.js) - Google Ads: 806267889 -->
+  <meta name="robots" content="noindex">
 <script async src="https://www.googletagmanager.com/gtag/js?id=AW-806267889"></script>
 <script>
   window.dataLayer = window.dataLayer || [];
