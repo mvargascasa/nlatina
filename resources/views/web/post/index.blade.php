bfc6abf45621a4735b6421e1ba8c0490c5337c4b
sebastian8080 <sebas25211@hotmail.com>
2022-06-20 11:39:58 -0500

    Agregando reading_time en el index del blog y cambiando de color los links paginator

--- resources/views/web/post/index.blade.php
+++ resources/views/web/post/index.blade.php
@@ -11,6 +11,10 @@
 <meta property="og:title"       content="Blog de Noticias e Información sobre Trámites Consulares en New York" />
 <meta property="og:description" content="Noticias e Información para Ciudadanos de Latinos en New York, Trámites Consulares, Apostillas, Poderes, Renovación de Pasaportes." />
 <meta property="og:image"       content="https://notarialatina.com/img/meta-notaria-latina-queens-new-york.jpg" />
+
+<style>
+    .pagination li.active .page-link, .pagination li .page-link:hover {background-color: #122944;color: #ffffff;font-weight: bold;}
+</style>
 @endsection
 
 @section('phoneNumberHidden', '+18007428602')
@@ -40,26 +44,42 @@
         <div class="row">
 
             @foreach ($posts as $post)
-                <div class="col-12 col-md-4">
-                    <div class="card my-2">
+                <div class="col-12 col-md-4 mb-4">
+                    <div class="card my-2 h-100">
                         <a href="{{route('post.slug',$post->slug)}}" class="stretched-link">
-                            <img data-src="{{url('uploads/'.$post->imgdir)}}" class="lazy card-img-top" alt="Imagen {{ $post->name }}" style="object-fit: cover;height: 150px !important;">
+                            <img data-src="https://notarialatina.com/uploads/{{$post->imgdir}}" class="lazy card-img-top" alt="Imagen {{ $post->name }}" style="object-fit: cover;height: 150px !important;">
+                            {{-- {{url('uploads/'.$post->imgdir)}} --}}
                         </a>
                         <div class="card-body p-2" style="position:relative;">
-                        <span class="d-block text-muted font-weight-bold text-truncate "
+                        <span class="d-block text-muted font-weight-bold"
                                 style="font-size:1rem">{{$post->name}}</span>
                         <span class="d-block text-muted text-truncate">
                             <?php echo strip_tags(substr($post->body,0,300))  ?>
                         </span>
-                        <div class="small text-muted float-left">{{$post->created_at->format('M d')}}</div>
+                    </div>
+                    <div class="card-footer bg-white" style="border: none">
+                        <div class="small text-muted float-left">
+                            {{-- <img class="lazy" width="20" height="20" data-src="{{ asset('img/calendar.webp') }}" alt="{{$post->name}}">  --}}
+                            <i class="far fa-calendar-alt" style="font-size: 17px"></i>
+                            {{$post->created_at->format('M d, Y')}}
                         </div>
+                        <div class="small text-muted float-right">
+                            <p class="d-flex align-items-center">
+                                {{-- <img class="lazy mr-1" width="20" height="20" data-src="{{ asset('img/reloj.webp') }}" alt="{{ $post->name}}">  --}}
+                                <i class="far fa-clock mr-1" style="font-size: 17px"></i>
+                                {{ $post->reading_time}} min.
+                            </p>
+                        </div>    
+                    </div>
                     </div>
                 </div>
             @endforeach
 
         </div>
         
-        {{$posts->links()}}
+        <div class="d-flex justify-content-center">
+            {{$posts->links()}}
+        </div>
 </div>
 
 
