f158a201bb45fdd72ac478924c5b7d2c2d1ec66c
sebastian8080 <sebas25211@hotmail.com>
2022-06-22 17:27:24 -0500

    Agregando icon de wpp para compartir el post

--- resources/views/web/post/show.blade.php
+++ resources/views/web/post/show.blade.php
@@ -69,6 +69,7 @@
                 <div class="col-sm-12 d-flex justify-content-center">
                     <a id="shareToFacebook" href=""><i class="fab fa-facebook fa-2x"></i></a>
                     <a id="shareToTwitter" href=""><i class="fab fa-twitter fa-2x ml-4"></i></a>
+                    <a id="shareToWpp" href=""><i class="fab fa-whatsapp fa-2x ml-4" style="color: #25D366"></i></a>
                 </div>
             </div>
         </div>
@@ -165,7 +166,7 @@
         let shareLink = window.location.href;
         document.getElementById('shareToFacebook').addEventListener('click', () => {window.open('https://www.facebook.com/sharer/sharer.php?u=' + shareLink, 'facebook-share-dialog', 'width=626, height=436');});
         document.getElementById('shareToTwitter').addEventListener('click', () => {window.open('https://twitter.com/intent/tweet?url='+shareLink)});
-
+        document.getElementById('shareToWpp').addEventListener('click', () => {window.open('https://api.whatsapp.com/send?text='+shareLink, '_blank')});
         document.addEventListener("DOMContentLoaded",function(){var e;if("IntersectionObserver"in window){e=document.querySelectorAll(".lazy");var n=new IntersectionObserver(function(e,t){e.forEach(function(e){if(e.isIntersecting){var t=e.target;t.src=t.dataset.src,t.classList.remove("lazy"),n.unobserve(t)}})});e.forEach(function(e){n.observe(e)})}else{var t;function r(){t&&clearTimeout(t),t=setTimeout(function(){var n=window.pageYOffset;e.forEach(function(e){e.offsetTop<window.innerHeight+n&&(e.src=e.dataset.src,e.classList.remove("lazy"))}),0==e.length&&(document.removeEventListener("scroll",r),window.removeEventListener("resize",r),window.removeEventListener("orientationChange",r))},20)}e=document.querySelectorAll(".lazy"),document.addEventListener("scroll",r),window.addEventListener("resize",r),window.addEventListener("orientationChange",r)}});
     </script>
 @endsection
