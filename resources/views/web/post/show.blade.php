e2d372560b04f2acb9da10ed0f33971011c9a36b
sebastian8080 <sebas25211@hotmail.com>
2022-06-22 16:59:29 -0500

    Agregando icono de facebook para compartir post

--- resources/views/web/post/show.blade.php
+++ resources/views/web/post/show.blade.php
@@ -162,7 +162,6 @@
         });
         document.getElementById('shareToFacebook').addEventListener('click', () => {
             shareLink = window.location.href;
-            alert(shareLink);
             window.open('https://www.facebook.com/sharer/sharer.php?u=' + shareLink);
         });
         document.addEventListener("DOMContentLoaded",function(){var e;if("IntersectionObserver"in window){e=document.querySelectorAll(".lazy");var n=new IntersectionObserver(function(e,t){e.forEach(function(e){if(e.isIntersecting){var t=e.target;t.src=t.dataset.src,t.classList.remove("lazy"),n.unobserve(t)}})});e.forEach(function(e){n.observe(e)})}else{var t;function r(){t&&clearTimeout(t),t=setTimeout(function(){var n=window.pageYOffset;e.forEach(function(e){e.offsetTop<window.innerHeight+n&&(e.src=e.dataset.src,e.classList.remove("lazy"))}),0==e.length&&(document.removeEventListener("scroll",r),window.removeEventListener("resize",r),window.removeEventListener("orientationChange",r))},20)}e=document.querySelectorAll(".lazy"),document.addEventListener("scroll",r),window.addEventListener("resize",r),window.addEventListener("orientationChange",r)}});
