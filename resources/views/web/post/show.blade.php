ec8b2c0ea49bb0d3bcb519a24cf0a5966f216f7a
sebastian8080 <sebas25211@hotmail.com>
2022-06-23 10:21:02 -0500

    Agregando box shadow en las card de post relacionados

--- resources/views/web/post/show.blade.php
+++ resources/views/web/post/show.blade.php
@@ -26,6 +26,7 @@
             h2{font-size: 20px !important; font-weight: 600 !important}
             #benefits{margin-top:25px !important}
         }
+        .card:hover{box-shadow: rgba(50, 50, 93, 0.25) 0px 6px 12px -2px, rgba(0, 0, 0, 0.3) 0px 3px 7px -3px;}
     </style>
 @endsection
 
