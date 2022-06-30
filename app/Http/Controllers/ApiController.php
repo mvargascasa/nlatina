c16e1bf0b8d22168cc155997dfdfdec268e038d1
sebastian8080 <sebas25211@hotmail.com>
2022-06-30 11:17:15 -0500

    Retornando atributo codigo_pais y phone concatenados en la api de los partners

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -4,15 +4,16 @@ namespace App\Http\Controllers;
 
 use App\Partner;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class ApiController extends Controller
 {
     public function showallpartners(){
 
-        //return Partner::with('specialties')->get();
+        $partners = Partner::select('id', 'name', 'lastname', DB::raw("CONCAT(partners.codigo_pais, '', partners.phone) AS phone_number"), 'email', 'country_residence', 'state', 'city')
+                    ->with('specialties:name_specialty')
+                    ->get();
 
-        return Partner::select('id', 'name', 'lastname', 'phone', 'email', 'country_residence', 'state', 'city')
-                        ->with('specialties:name_specialty')
-                        ->get();
+        return $partners;
     }
 }
