87f0b147eeb2381d485a4eb50a98671e6edf7e43
sebastian8080 <sebas25211@hotmail.com>
2022-06-30 09:33:47 -0500

    Agregando middleware ApiKeyValidate para llave en el header cuando hacen una peticion a la api de partners

---
+++ app/Http/Middleware/ApiKeyValidate.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+
+class ApiKeyValidate
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+
+        $key = $request->headers->get('api-key');
+
+        // if(isset($key) == "prueba"){
+        //     return $next($request);
+        // } else {
+        //     return response()->json([
+        //         'error' => 'unauthorized'
+        //     ], 401);
+        // }
+
+        if(isset($key) && $key === "partnersNL*"){
+            return $next($request);
+        } else {
+            return response()->json([
+                'error' => 'unauthorized',
+              ], 401);
+        }
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -18,4 +18,8 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('partners', 'ApiController@showallpartners');
+Route::group(["middleware" => "apikey.validate"], function () {
+
+    Route::get('partners', 'ApiController@showallpartners');
+    
+});
