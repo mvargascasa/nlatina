097915fdf5c838c4de44a8c719684b1ece5b6863
sebastian8080 <sebas25211@hotmail.com>
2022-07-12 09:50:25 -0500

    Quitando js que carga el jquery de la pagina de apostillas

--- resources/views/web/office/apostillas.blade.php
+++ resources/views/web/office/apostillas.blade.php
@@ -141,10 +141,10 @@ aria-hidden="true">
   scriptPopper.src = "{{ asset('js/popper.min.js') }}";
     window.addEventListener('load', (event) => {
         document.getElementById('prisection').style.backgroundImage = "url('../img/inicio.jpg')";
-        document.getElementsByTagName("script")[0].parentNode.appendChild(scriptPopper);
-        document.getElementsByTagName("script")[0].parentNode.appendChild(scriptBootstrap);
+        // document.getElementsByTagName("script")[0].parentNode.appendChild(scriptPopper);
+        // document.getElementsByTagName("script")[0].parentNode.appendChild(scriptBootstrap);
     });
-    document.addEventListener("DOMContentLoaded",function(){var e;if("IntersectionObserver"in window){e=document.querySelectorAll(".lazy");var n=new IntersectionObserver(function(e,t){e.forEach(function(e){if(e.isIntersecting){var t=e.target;t.src=t.dataset.src,t.classList.remove("lazy"),n.unobserve(t)}})});e.forEach(function(e){n.observe(e)})}else{var t;function r(){t&&clearTimeout(t),t=setTimeout(function(){var n=window.pageYOffset;e.forEach(function(e){e.offsetTop<window.innerHeight+n&&(e.src=e.dataset.src,e.classList.remove("lazy"))}),0==e.length&&(document.removeEventListener("scroll",r),window.removeEventListener("resize",r),window.removeEventListener("orientationChange",r))},20)}e=document.querySelectorAll(".lazy"),document.addEventListener("scroll",r),window.addEventListener("resize",r),window.addEventListener("orientationChange",r)}});
+    //document.addEventListener("DOMContentLoaded",function(){var e;if("IntersectionObserver"in window){e=document.querySelectorAll(".lazy");var n=new IntersectionObserver(function(e,t){e.forEach(function(e){if(e.isIntersecting){var t=e.target;t.src=t.dataset.src,t.classList.remove("lazy"),n.unobserve(t)}})});e.forEach(function(e){n.observe(e)})}else{var t;function r(){t&&clearTimeout(t),t=setTimeout(function(){var n=window.pageYOffset;e.forEach(function(e){e.offsetTop<window.innerHeight+n&&(e.src=e.dataset.src,e.classList.remove("lazy"))}),0==e.length&&(document.removeEventListener("scroll",r),window.removeEventListener("resize",r),window.removeEventListener("orientationChange",r))},20)}e=document.querySelectorAll(".lazy"),document.addEventListener("scroll",r),window.addEventListener("resize",r),window.addEventListener("orientationChange",r)}});
   </script>
 @endsection
 
