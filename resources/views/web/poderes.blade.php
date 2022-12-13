57a7e4e69c1478ab38ed6387fe6e25480b0a73f9
sebastian8080 <sebas25211@hotmail.com>
2022-12-13 14:03:40 -0500

    quitando video

--- resources/views/web/poderes.blade.php
+++ resources/views/web/poderes.blade.php
@@ -39,7 +39,6 @@
   setTimeout(() => {
     document.getElementById('scriptrecaptcha').src = "https://www.google.com/recaptcha/api.js?render=6LdI9cMeAAAAALgxUrh7mzlzFBlIV-F4Gzvbp2D8"; 
       //console.log('cargando script recaptcha...');
-    document.getElementById('iframevideo').src="https://www.youtube.com/embed/AHE8EC0wsNA";
   }, 3000);
 
   setTimeout(() => {
@@ -87,9 +86,9 @@
     </div>
   </section>
 
-  <section style="width:100%; text-align:center; margin-top:10px;">
+  {{-- <section style="width:100%; text-align:center; margin-top:10px;">
     <iframe id="iframevideo" width="560" height="315" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-  </section>
+  </section> --}}
 
 {{-- <div class="container pt-4">
             <h2 style="font-weight: 500; font-size: 28px">¿Qué es un poder notarial?</h2>
