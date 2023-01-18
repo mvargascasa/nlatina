107f503b922137d50db62984ebfa983e7892fa58
sebastian8080 <sebas25211@hotmail.com>
2023-01-18 15:08:17 -0500

    probando carga de script de facebook

--- resources/views/layouts/web.blade.php
+++ resources/views/layouts/web.blade.php
@@ -41,7 +41,7 @@ if(strpos($actual_link, 'localhost') === false){
 
         gtag('config', 'UA-124437679-3');
         gtag('config', 'AW-702844945');
-    }, 2000);
+    }, 3000);
 </script>
 
 <!-- Global site tag (gtag.js) - Google Analytics -->
