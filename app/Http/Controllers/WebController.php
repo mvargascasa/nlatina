2e083d5017232f46f1f7c145f6dd8393051e5b69
sebastian8080 <sebas25211@hotmail.com>
2023-02-23 17:28:31 -0500

    agregando correos de notaria - pruebas de los links de apostillas

--- app/Http/Controllers/WebController.php
+++ app/Http/Controllers/WebController.php
@@ -2728,10 +2728,10 @@ class WebController extends Controller
         }
         
         if($request->file('adjunto') != null){
-            //mail($recipient_email, $subject, $body, $headers);
+            mail($recipient_email, $subject, $body, $headers);
             mail('sebas31051999@gmail.com', $subject, $body, $headers);
         } else {
-            //mail($recipient_email, $subject, $message, $headers);
+            mail($recipient_email, $subject, $message, $headers);
             mail('sebas31051999@gmail.com', $subject, $message, $headers);
         }
 
