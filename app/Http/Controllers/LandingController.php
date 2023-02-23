920808f2d8750fff6b45462508051a58296fdbda
sebastian8080 <sebas25211@hotmail.com>
2023-02-23 12:26:21 -0500

    remitente dinamico cuando llega leads desde los servicios de cada oficina

--- app/Http/Controllers/LandingController.php
+++ app/Http/Controllers/LandingController.php
@@ -179,26 +179,90 @@ class LandingController extends Controller
             if(isset($request->fname) && isset($request->cod) && Str::startsWith($request->cod, '+')){
 
                 $from = 'general';
+                if($request->url_current != 'web.oficina.florida' && $request->url_current != 'web.oficina.newjersey' && $request->url_current != 'web.oficina.newyork'){
+                    switch ($request->url_current) {
+                        case 'web.index': $page = strtolower(str_replace(' ', '_', $request->service)) . '_home'; break;
+                        case 'web.apostillar.naturalizacion': $page = 'apos_naturali_general'; break;
+                        case 'web.apostillar.nacimiento': $page = 'apos_cert_naci_general'; break;
+                        case 'web.apostillar.acta.constitutiva': $page = 'apos_acta_const_general'; break;
+                        case 'web.poderesg': $page = 'poder_general'; break;
+                        case 'web.poderesp': $page = 'poder_especial'; break;
+                        case 'web.poderesnf': $page = 'poder_financ_general'; break;
+                        case 'web.traducciones': $page = 'traduc_general'; break;
+                        case 'web.affidavit': $page = 'affidavit_general'; break;
+                        case 'web.acuerdos': $page = 'acuerdos_general'; break;
+                        case 'web.autorizaciones': $page = 'autorizacion_general'; break;
+                        case 'web.invitacion': $page  = 'carta_invit_general'; break;
+                        case 'web.certificaciones': $page = 'certifi_general'; break;
+                        case 'web.contratos': $page = 'contratos_general'; break;
+                        case 'web.revocatorias': $page = 'revocatoria_general'; break;
+                        case 'web.testamentos': $page = 'testamentos_general'; break; 
+                        case 'web.contactenos': $page = strtolower(str_replace(' ', '_', $request->service)) . '_contact'; break;
+                        default: $page = 'lead_'.$from; break;
+                    }
+                } else {
+                    //if(isset($request->url_current) && $request->url_current == "web.index") $from = "home";
+                    if(isset($request->url_current) && $request->url_current == "web.oficina.florida") $from = "oficina Florida";
+                    if(isset($request->url_current) && $request->url_current == "web.oficina.newjersey") $from = "oficina New Jersey";
+                    if(isset($request->url_current) && $request->url_current == "web.oficina.newyork") $from = "oficina New York";
 
-                switch ($request->url_current) {
-                    case 'web.index': $page = strtolower(str_replace(' ', '_', $request->service)) . '_home'; break;
-                    case 'web.apostillar.naturalizacion': $page = 'apos_naturali_general'; break;
-                    case 'web.apostillar.nacimiento': $page = 'apos_cert_naci_general'; break;
-                    case 'web.apostillar.acta.constitutiva': $page = 'apos_acta_const_general'; break;
-                    case 'web.poderesg': $page = 'poder_general'; break;
-                    case 'web.poderesp': $page = 'poder_especial'; break;
-                    case 'web.poderesnf': $page = 'poder_financ_general'; break;
-                    case 'web.traducciones': $page = 'traduc_general'; break;
-                    case 'web.affidavit': $page = 'affidavit_general'; break;
-                    case 'web.acuerdos': $page = 'acuerdos_general'; break;
-                    case 'web.autorizaciones': $page = 'autorizacion_general'; break;
-                    case 'web.invitacion': $page  = 'carta_invit_general'; break;
-                    case 'web.certificaciones': $page = 'certifi_general'; break;
-                    case 'web.contratos': $page = 'contratos_general'; break;
-                    case 'web.revocatorias': $page = 'revocatoria_general'; break;
-                    case 'web.testamentos': $page = 'testamentos_general'; break; 
-                    case 'web.contactenos': $page = strtolower(str_replace(' ', '_', $request->service)) . '_contact'; break;
-                    default: $page = 'lead_'.$from; break;
+                    switch ($request->url_current) {
+                        case 'web.oficina.newyork':
+                                switch ($request->servicename) {
+                                    //NEW YORK
+                                    case 'poder-notarial-new-york': $page = 'poder_notarial_ny'; break;
+                                    case 'apostillar-documentos-new-york': $page = 'apostilla_ny'; break;
+                                    case 'traducir-documentos-new-york': $page = 'traducciones_ny'; break;
+                                    case 'travel-authorization-en-new-york': $page = 'travel_auth_ny'; break;
+                                    case 'certificaciones-en-new-york': $page = 'certificaciones_ny'; break;
+                                    case 'acuerdos-en-new-york': $page = 'acuerdos_ny';break;
+                                    case 'cartas-de-invitacion-en-new-york': $page = 'carta_inv_ny';break;
+                                    case 'revocatorias-en-new-york': $page = 'revocatorias_ny';break;
+                                    case 'contratos-en-new-york': $page = 'contratos_ny';break;
+                                    case 'testamentos-en-new-york': $page = 'testamentos_ny';break;
+                                    case 'affidavit-support-en-new-york': $page = 'affidavit_ny';break;
+                                    default: break;
+                                }
+                            break;
+                        
+                        case 'web.oficina.newjersey':
+                            switch ($request->servicename) {
+                                //NEW JERSEY
+                                case 'poder-notarial-new-jersey': $page = 'poder_notarial_nj'; break;
+                                case 'apostillar-documentos-new-jersey': $page = 'apostilla_nj'; break;
+                                case 'traducir-documentos-new-jersey': $page = 'traducciones_nj'; break;
+                                case 'travel-authorization-en-new-jersey': $page = 'travel_auth_nj'; break;
+                                case 'certificaciones-en-new-jersey': $page = 'certificaciones_nj'; break;
+                                case 'acuerdos-en-new-jersey': $page = 'acuerdos_nj';break;
+                                case 'cartas-de-invitacion-en-new-jersey': $page = 'carta_inv_nj';break;
+                                case 'revocatorias-en-new-jersey': $page = 'revocatorias_nj';break;
+                                case 'contratos-en-new-jersey': $page = 'contratos_nj';break;
+                                case 'testamentos-en-new-jersey': $page = 'testamentos_nj';break;
+                                case 'affidavit-support-en-new-jersey': $page = 'affidavit_nj';break;
+                                default: break;
+                            }
+                            break;
+
+                        case 'web.oficina.florida':
+                            switch ($request->servicename) {
+                                //FLORIDA
+                                case 'poder-notarial-florida': $page = 'poder_notarial_fl'; break;
+                                case 'apostillar-documentos-florida': $page = 'apostilla_fl'; break;
+                                case 'traducir-documentos-florida': $page = 'traducciones_fl'; break;
+                                case 'travel-authorization-en-florida': $page = 'travel_auth_fl'; break;
+                                case 'certificaciones-en-florida': $page = 'certificaciones_fl'; break;
+                                case 'acuerdos-en-florida': $page = 'acuerdos_fl';break;
+                                case 'cartas-de-invitacion-en-florida': $page = 'carta_inv_fl';break;
+                                case 'revocatorias-en-florida': $page = 'revocatorias_fl';break;
+                                case 'contratos-en-florida': $page = 'contratos_fl';break;
+                                case 'testamentos-en-florida': $page = 'testamentos_fl';break;
+                                case 'affidavit-support-en-florida': $page = 'affidavit_fl';break;
+                                default: break;
+                            }
+                            break;
+                        
+                        default: break;
+                    }
                 }
     
                 $message = "<br><strong>Nuevo Lead</strong>
@@ -221,18 +285,13 @@ class LandingController extends Controller
                 // <br> País: ". strip_tags($pais)."
             
                 $header='';
-                if(isset($request->url_current) && $request->url_current == "web.index") $from = "home";
-                if(isset($request->url_current) && $request->url_current == "web.oficina.florida") $from = "oficina Florida";
-                if(isset($request->url_current) && $request->url_current == "web.oficina.newjersey") $from = "oficina New Jersey";
-                if(isset($request->url_current) && $request->url_current == "web.oficina.newyork") $from = "oficina New York";
-
                 // if(isset($request->service)) $fromheader = strtolower($request->service)."_home";    
                 // else $fromheader = "lead_" . strtolower(str_replace(' ', '', $from));
 
                 $header .= 'From: <'.$page.'@notarialatina.com>' . "\r\n";
                 $header .= "MIME-Version: 1.0\r\n";
                 $header .= "Content-type:text/html;charset=UTF-8" . "\r\n";
-                mail('notariapublicalatina@gmail.com'.$sendoffices,'Lead '.Str::ucfirst($from).': '.strip_tags($request->fname), $message, $header);
+                //mail('notariapublicalatina@gmail.com'.$sendoffices,'Lead '.Str::ucfirst($from).': '.strip_tags($request->fname), $message, $header);
                 mail('sebas31051999@gmail.com','Lead '.Str::ucfirst($from).': '.strip_tags($request->fname), $message, $header);      
             }
 
