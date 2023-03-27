c0a275487268ae2d0d91c40894852d6cdc721656
sebastian8080 <sebas25211@hotmail.com>
2023-03-27 16:07:45 -0500

    agregando views en partner de notaria

--- resources/views/web/partner.blade.php
+++ resources/views/web/partner.blade.php
@@ -275,11 +275,11 @@
                 <p class="bg-blue text-white p-2 w-auto text-center font-weight-bold" style="letter-spacing: 15px; border-radius: 0px 25px 25px 0px">{{strtoupper($partner->company)}}</p>
                 <div class="row mt-4">
                     <div class="col-sm-6">
-                        <p class="pt-2 txt-blue" style="letter-spacing: 15px">PUNTUACIÓN</p>
+                        <p class="pt-2 txt-blue float-right" style="letter-spacing: 15px">PUNTUACIÓN</p>
                     </div>
                     @php $rating = $partner->averageRating(); @endphp
                     <div class="col-sm-6">
-                        <div data-toggle="modal" data-target="#exampleModalCenter" style="color: #9A7A2E; cursor: pointer">
+                        <div data-toggle="modal" data-target="#exampleModalCenter" style="color: #FEC02F; cursor: pointer">
                             @foreach(range(1,5) as $i)
                                 <span class="fa-stack" style="width:2em" onclick="openModalRating();">
                                     <i class="far fa-star fa-stack-2x"></i>
@@ -295,6 +295,22 @@
                             @endforeach
                         </div>
                     </div>
+                </div>
+                <div class="row mt-3">
+                    @if(count($partner->customers) > 0)
+                        <div class="col-sm-12">
+                            <div style="color: #FEC02F">
+                                <p style="letter-spacing: 1px"><i class="fas fa-search"></i> {{count($partner->customers)}} @if(count($partner->customers) == 1) PERSONA HA @else PERSONAS HAN @endif REALIZADO UNA CONSULTA</p>
+                            </div>
+                        </div>
+                    @endif
+                    @if($partner->views > 0)
+                    <div class="col-sm-12">
+                        <div style="color: #FEC02F">
+                            <p style="letter-spacing: 1px"><i class="fas fa-eye"></i> {{$partner->views+1}} VISUALIZACIONES AL PERFIL</p>
+                        </div>
+                    </div>
+                    @endif
                 </div>
             </div>
         </div>
