c2a986e008932ad485d0720317b372a1027f1a41
sebastian8080 <sebas25211@hotmail.com>
2023-04-05 14:45:24 -0500

    replace anchor tag from carousel buttons arrow right and left to label tag

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -335,12 +335,12 @@
 {{-- @endif --}}
 
     </div>
-    <a class="carousel-control-prev" href="#carouselExampleCaptions" role="button" data-slide="prev">
+    <label style="cursor: pointer" class="carousel-control-prev" href="#carouselExampleCaptions" role="button" data-slide="prev">
       <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-    </a>
-    <a class="carousel-control-next" href="#carouselExampleCaptions" role="button" data-slide="next">
+    </label>
+    <label style="cursor: pointer" class="carousel-control-next" href="#carouselExampleCaptions" role="button" data-slide="next">
       <span class="carousel-control-next-icon" aria-hidden="true"></span>
-    </a>
+    </label>
   </div>
 
   <div class="p-2" style="position: absolute;right: 20px;top: 80px;z-index: 999">
