25e641c97bfdd3f44866b86eb3fc758b3374d299
sebastian8080 <sebas25211@hotmail.com>
2023-04-06 15:02:55 -0500

    change z-form to serv-form

--- resources/views/web/traducciones.blade.php
+++ resources/views/web/traducciones.blade.php
@@ -146,7 +146,7 @@
       <div class="text-white rounded p-4 mb-4 shadow sticky" style="background-color: #2B384D">
         <p class="text-center h6">¿Necesita realizar la traducción de un documento?</p>
         <p class="text-center" style="font-size: 14px"><i class="fas fa-arrow-down text-warning"></i> Solicite su trámite en línea <i class="fas fa-arrow-down text-warning"></i></p>
-        @include('z-form')
+        @include('web.serv-form')
       </div>
     </div>
   </div>
@@ -164,7 +164,7 @@ aria-hidden="true">
          </button>
        </div>
        <div class="modal-body">
-         @include('web.serv-form')
+         @include('z-form')
        </div>
      </div>
    </div>
