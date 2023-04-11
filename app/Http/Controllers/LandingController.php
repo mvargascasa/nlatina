4d8d859ee2a88eae80cb2dd3726a5a2c52606fd6
sebastian8080 <sebas25211@hotmail.com>
2023-04-11 11:39:31 -0500

    probando envio

--- app/Http/Controllers/LandingController.php
+++ app/Http/Controllers/LandingController.php
@@ -339,8 +339,7 @@ class LandingController extends Controller
                 <br><b> Email: </b> " . strip_tags($request->email) ."
                 <br><b> Servicio: </b> ".strip_tags($servicename)."
                 <br><b> Mensaje: </b> ".strip_tags($request->message)."
-                <br><b> Fuente: </b> GoogleAds
-                <br><b> Página: </b> " . url()->previous();
+                <br><b> Fuente: </b> GoogleAds";
 
                 // <br><b> Estado: </b> " . strip_tags($request->state) . "
                 // <br><b> Telef: </b> ". strip_tags($request->get('cod_pais')) . " " . strip_tags($request->bbb)."
