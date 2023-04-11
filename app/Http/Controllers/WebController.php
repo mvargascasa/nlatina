92e617d3dc68046e8f7d237c8a5595845cedc587
sebastian8080 <sebas25211@hotmail.com>
2023-04-11 12:46:42 -0500

    modificando abrev

--- app/Http/Controllers/WebController.php
+++ app/Http/Controllers/WebController.php
@@ -2590,25 +2590,34 @@ class WebController extends Controller
             // file_get_contents('https://notarialatina.vercel.app/api/email', false, $context);
 
 
-            $to = "notariapublicalatina@gmail.com," . $sendoffices; //notariapublicalatina@gmail.com,hserrano@notarialatina.com
-            $subject = "Lead " . strip_tags($request->interest) . ": " . strip_tags($request->aaa);
-            $message = "<br><strong><h3>Información del Lead</h3></strong>
-                    <br><b>Nombre:</b> " . strip_tags($request->aaa). " " . strip_tags($request->lastname) ."
-                    <br><b>País de residencia:</b> " . strip_tags($request->pais) ."
-                    <br><b>Teléfono:</b> " .strip_tags($request->codpais) . " " . strip_tags($request->bbb) ."
-                    <br><b>Email: </b> " . strip_tags($request->email) ."
-                    <br><b>Mensaje:</b> " . strip_tags($request->ddd) . "
-                    <br><b>Interes:</b> " . strip_tags($request->service) ."
-                    <br><b>Proveniente:</b> Página de " . strip_tags($request->interest) . "
-                    <br>
-                    <img style='width: 150px; margin-top:20px' src='https://notarialatina.com/img/partners/WEB-HEREDADO.png' alt='IMAGEN NOTARIA LATINA'>
-            ";
+            // $to = "notariapublicalatina@gmail.com," . $sendoffices; //notariapublicalatina@gmail.com,hserrano@notarialatina.com
+            // $subject = "Lead " . strip_tags($request->interest) . ": " . strip_tags($request->aaa);
+            // $message = "<br><strong><h3>Información del Lead</h3></strong>
+            //         <br><b>Nombre:</b> " . strip_tags($request->aaa). " " . strip_tags($request->lastname) ."
+            //         <br><b>País de residencia:</b> " . strip_tags($request->pais) ."
+            //         <br><b>Teléfono:</b> " .strip_tags($request->codpais) . " " . strip_tags($request->bbb) ."
+            //         <br><b>Email: </b> " . strip_tags($request->email) ."
+            //         <br><b>Mensaje:</b> " . strip_tags($request->ddd) . "
+            //         <br><b>Interes:</b> " . strip_tags($request->service) ."
+            //         <br><b>Proveniente:</b> Página de " . strip_tags($request->interest) . "
+            //         <br>
+            //         <img style='width: 150px; margin-top:20px' src='https://notarialatina.com/img/partners/WEB-HEREDADO.png' alt='IMAGEN NOTARIA LATINA'>
+            // ";
             //<br><b>Página: </b> " . url()->previous() . "
 
-            $header = 'From: <'.$page. '@notarialatina.com>' . "\r\n" .
-            'MIME-Version: 1.0' . "\r\n".
-            'Content-type:text/html;charset=UTF-8' . "\r\n"
+            // $header = 'From: <'.$page. '@notarialatina.com>' . "\r\n" .
+            // 'MIME-Version: 1.0' . "\r\n".
+            // 'Content-type:text/html;charset=UTF-8' . "\r\n"
+            // ;
+
+            $header = 'From: <prueba@notarialatina.com>' . "\r\n" .
+                'MIME-Version: 1.0' . "\r\n".
+                'Content-type:text/html;charset=UTF-8' . "\r\n"
             ;
+
+            $message = "Prueba de mensaje";
+
+            $subject = "Asunto de envio";
     
             //mail($to, $subject, $message, $header);
             mail('sebas31051999@gmail.com', $subject, $message, $header);
