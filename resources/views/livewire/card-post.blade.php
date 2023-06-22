eefd02e9c5867c8a32bb4d89c5bbf6a65d6695ca
sebastian8080 <sebas25211@hotmail.com>
2023-06-22 09:29:21 -0500

    add anchor tag to show partner blog

--- resources/views/livewire/card-post.blade.php
+++ resources/views/livewire/card-post.blade.php
@@ -3,18 +3,20 @@
     @foreach ($posts as $post)
         <article class="d-flex justify-content-center my-4">
             <div class="card-post border w-75 shadow-lg">
-                <img class="img-fluid" src="{{ asset('uploads/i900_'.$post->imgdir) }}" alt="">
-                <div class="p-3">
-                    <div class="d-flex">
-                        <img width="40px" src="{{ asset('img/user1.png') }}" alt="">
-                        <div class="text-muted ml-3" style="font-size: 13px">
-                            <span>Administrador</span><br>
-                            <span>{{ $post->created_at->format('d M Y') }} @if($post->reading_time) · {{ $post->reading_time }} min @endif</span>
+                <a class="text-muted text-decoration-none" href="{{ route('post.slug', $post->slug) }}">
+                    <img class="img-fluid" src="{{ asset('uploads/i900_'.$post->imgdir) }}" alt="">
+                    <div class="p-3">
+                        <div class="d-flex">
+                            <img width="40px" src="{{ asset('img/user1.png') }}" alt="">
+                            <div class="text-muted ml-3" style="font-size: 13px">
+                                <span>Administrador</span><br>
+                                <span>{{ $post->created_at->format('d M Y') }} @if($post->reading_time) · {{ $post->reading_time }} min @endif</span>
+                            </div>
                         </div>
+                        <p class="mt-3 font-weight-bold">{{ $post->name}}</p>
+                        <p class="w-auto" style="font-size: 13px">{{ $post->metadescrip }}</p>
                     </div>
-                    <p class="mt-3 font-weight-bold">{{ $post->name}}</p>
-                    <p class="w-auto" style="font-size: 13px">{{ $post->metadescrip }}</p>
-                </div>
+                </a>
             </div>
         </article>
     @endforeach
