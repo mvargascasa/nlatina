3f2be79ed93088a0781dacda05fa903a0cc3f565
sebastian8080 <sebas25211@hotmail.com>
2023-07-19 11:52:07 -0500

    filter function

--- app/Http/Livewire/ListPartners.php
+++ app/Http/Livewire/ListPartners.php
@@ -6,6 +6,7 @@ use Livewire\Component;
 use App\Partner;
 use Livewire\WithPagination;
 use Carbon\Carbon;
+use Illuminate\Http\Request;
 
 class ListPartners extends Component
 {
@@ -13,12 +14,7 @@ class ListPartners extends Component
 
     protected $paginationTheme = 'bootstrap';
 
-    public $name;
-    public $lastname = "";
-    public $from_date_created = "";
-    public $to_date_created = "";
-    public $from_date_publicated = "";
-    public $to_date_publicated = "";
+    public $name, $lastname, $from_date_created, $to_date_created, $from_date_publicated, $to_date_publicated;
 
     public function clean(){
         $this->name = "";
@@ -29,14 +25,18 @@ class ListPartners extends Component
         $this->to_date_publicated = "";
     }
 
-
     public function render()
     {
-        $currentDate = Carbon::now()->addDay();
+        $partners_filter = Partner::orderBy('id', 'desc');
 
-        return view('livewire.list-partners', [
-            'partners' => Partner::where('name', 'LIKE', '%'.$this->name.'%')->where('lastname', 'LIKE', '%'.$this->lastname.'%')->whereBetween('created_at', [$this->from_date_created ? $this->from_date_created : "2021-11-01", $this->to_date_created ? $this->to_date_created : $currentDate])->whereBetween('fecha_publicado', [$this->from_date_publicated ? $this->from_date_publicated : "2021-11-01", $this->to_date_publicated ? $this->to_date_publicated : $currentDate])->paginate(10),
-            'total_partners' => Partner::where('name', 'LIKE', '%'.$this->name.'%')->where('lastname', 'LIKE', '%'.$this->lastname.'%')->whereBetween('created_at', [$this->from_date_created ? $this->from_date_created : "2021-11-01", $this->to_date_created ? $this->to_date_created : $currentDate])->whereBetween('fecha_publicado', [$this->from_date_publicated ? $this->from_date_publicated : "2021-11-01", $this->to_date_publicated ? $this->to_date_publicated : $currentDate])->count(),
-        ]);
+        if($this->name) $partners_filter->where('name', 'LIKE', '%'.$this->name.'%');
+        if($this->lastname) $partners_filter->where('lastname', 'LIKE', '%'.$this->lastname.'%');
+        if($this->from_date_created || $this->to_date_created) $partners_filter->whereBetween('created_at', [$this->from_date_created, $this->to_date_created]);
+        if($this->from_date_publicated || $this->to_date_publicated) $partners_filter->whereBetween('fecha_publicado', [$this->from_date_publicated, $this->to_date_publicated]);
+        
+        $partners = $partners_filter->paginate(10);
+        $total_partners = $partners->total();
+
+        return view('livewire.list-partners', compact('partners', 'total_partners'));
     }
 }
