375d88074b231d4fd23f394380d4265ecc13e4eb
sebastian8080 <sebas25211@hotmail.com>
2023-08-16 10:55:36 -0500

    agregando script de tag manager

--- resources/views/landing/general.blade.php
+++ resources/views/landing/general.blade.php
@@ -122,6 +122,15 @@
     gtag('config', 'G-VJK9KRV3TL');
 </script> --}}
 
+<!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+  new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+  j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+  'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+  })(window,document,'script','dataLayer','GTM-KHSFW5X3');</script>
+  <!-- End Google Tag Manager -->
+  
+
 @if($oficina == "New York")
   <!--NEW YORK-->
   <!-- Google tag (gtag.js) --> 
@@ -267,6 +276,11 @@
   </style>
 </head>
 <body>
+
+  <!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KHSFW5X3"
+  height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+  <!-- End Google Tag Manager (noscript) -->
 
   <!-- Google Tag Manager (noscript) -->
   <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-NXP3WCV"
