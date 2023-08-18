55ce418cb90f51933df03baa4ed5ef2a0877429a
sebastian8080 <sebas25211@hotmail.com>
2023-08-18 16:57:36 -0500

    cambiando botones de contacto fixed

--- resources/views/layouts/web.blade.php
+++ resources/views/layouts/web.blade.php
@@ -646,7 +646,7 @@ $consuls = \App\Consulate::select('country', 'slug')->orderBy('country')->get();
 
 <div class="contenedor">
     <button class="botonF1">
-      <span class="d-flex align-items-center justify-content-center"><img class="img-fluid" width="40px" style="filter: invert(100%)" src="{{ asset('img/atencion-al-cliente.png') }}" alt="contactar a notaria latina" title="Contactar a Notaria Latina"></span>
+      <span class="d-flex align-items-center justify-content-center"><img class="lazy" width="40px" height="40px" style="filter: invert(100%)" data-src="{{ asset('img/atencion-al-cliente.png') }}" alt="contactar a notaria latina" title="Contactar a Notaria Latina"></span>
     </button>
     <button class="btncontact botonF2">
         <a href="tel:@yield('phoneNumberHidden')">
