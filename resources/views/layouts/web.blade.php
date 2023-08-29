7e024631e3c21ae9157454b8d06461ba4fa37f00
sebastian8080 <sebas25211@hotmail.com>
2023-08-29 10:38:43 -0500

    posponiendo carga de facebook plugin chat

--- resources/views/layouts/web.blade.php
+++ resources/views/layouts/web.blade.php
@@ -670,27 +670,31 @@ $consuls = \App\Consulate::select('country', 'slug')->where('country', '!=', 'Ch
 </div>
 
 <script>
-  var chatbox = document.getElementById('fb-customer-chat');
-  chatbox.setAttribute("page_id", "408436099982353");
-  chatbox.setAttribute("attribution", "biz_inbox");
+    setTimeout(() => {
+        var chatbox = document.getElementById('fb-customer-chat');
+        chatbox.setAttribute("page_id", "408436099982353");
+        chatbox.setAttribute("attribution", "biz_inbox");
+    }, 3000);
 </script>
 
 <!-- Your SDK code -->
 <script>
-  window.fbAsyncInit = function() {
-    FB.init({
-      xfbml            : true,
-      version          : 'v17.0'
-    });
-  };
-
-  (function(d, s, id) {
-    var js, fjs = d.getElementsByTagName(s)[0];
-    if (d.getElementById(id)) return;
-    js = d.createElement(s); js.id = id;
-    js.src = 'https://connect.facebook.net/es_LA/sdk/xfbml.customerchat.js';
-    fjs.parentNode.insertBefore(js, fjs);
-  }(document, 'script', 'facebook-jssdk'));
+    setTimeout(() => {
+        window.fbAsyncInit = function() {
+          FB.init({
+            xfbml            : true,
+            version          : 'v17.0'
+          });
+        };
+      
+        (function(d, s, id) {
+          var js, fjs = d.getElementsByTagName(s)[0];
+          if (d.getElementById(id)) return;
+          js = d.createElement(s); js.id = id;
+          js.src = 'https://connect.facebook.net/es_LA/sdk/xfbml.customerchat.js';
+          fjs.parentNode.insertBefore(js, fjs);
+        }(document, 'script', 'facebook-jssdk'));
+    }, 3000);
 </script>
 
 {{-- <div>
