c398a48e018adad3cd82b2baf073d4f921cb08aa
sebastian8080 <sebas25211@hotmail.com>
2023-09-28 14:42:50 -0500

    cambiando text-color de politicas de privacidad

--- resources/views/landing/general.blade.php
+++ resources/views/landing/general.blade.php
@@ -584,7 +584,7 @@
   </a>
 </div>
 
-<footer class="text-center navfoot text-white py-3"> Copyright ©2020 Notaria Latina. All rights reserved. <a href="{{ route('web.politicas') }}">Politicas de Privacidad</a> </footer>
+<footer class="text-center navfoot text-white py-3"> Copyright ©2020 Notaria Latina. All rights reserved. <a class="text-white font-weight-bold" href="{{ route('web.politicas') }}">Politicas de Privacidad</a> </footer>
 
 <script>
   window.addEventListener('load', (event) => {
