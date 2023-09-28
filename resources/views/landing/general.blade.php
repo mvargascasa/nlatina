9d97a6f0a098b2c187ab2743eb6312d5ebcd11ac
sebastian8080 <sebas25211@hotmail.com>
2023-09-28 09:54:55 -0500

    agregando settimeout en carga de script de tag manager

--- resources/views/landing/general.blade.php
+++ resources/views/landing/general.blade.php
@@ -116,18 +116,26 @@
   }, 3000);
 </script>
 <script>
+  setTimeout(() => {
     window.dataLayer = window.dataLayer || [];
     function gtag(){dataLayer.push(arguments);}
     gtag('js', new Date());
     gtag('config', 'G-VJK9KRV3TL');
+  }, 3000);
 </script>
 
+
+
 <!-- Google Tag Manager -->
-<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-  new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-  j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-  'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-  })(window,document,'script','dataLayer','GTM-KHSFW5X3');</script>
+<script>
+  setTimeout(() => {
+    (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+      new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+      j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+      'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+      })(window,document,'script','dataLayer','GTM-KHSFW5X3');
+  }, 3000);
+</script>
   <!-- End Google Tag Manager -->
 
 
