7bd69eed0ee029c563a69e25bad9a23926ec54b2
sebastian8080 <sebas25211@hotmail.com>
2023-10-12 11:49:17 -0500

    postergando carga de script con funcion gtag

--- resources/views/landing/general.blade.php
+++ resources/views/landing/general.blade.php
@@ -265,7 +265,8 @@
 @if($tlfhidden == "19082249259")
   <script> 
     setTimeout(() => {
-      gtag('config', 'AW-306069230/Hz3ICPe-pckYEO79-JEB', { 'phone_conversion_number': '9082249259' }); 
+      gtag('config', 'AW-306069230/Hz3ICPe-pckYEO79-JEB', { 'phone_conversion_number': '9082249259' });
+      console.log('execute function');
     }, 3500);
   </script>
   <!-- Event snippet for IDG_NEWJERSEY_TRADUCCION_MOBIL_LLAMADA_BOTON conversion page In your html page, add the snippet and call gtag_report_conversion when someone clicks on the chosen link or button. --> 
