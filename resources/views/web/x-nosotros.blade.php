f9c33fd04ab12000badc9d1b11c2b31c5cd0eae5
sebastian8080 <sebas25211@hotmail.com>
2023-11-06 16:55:00 -0500

    cambiando disenio de pagina de nosotros

--- resources/views/web/x-nosotros.blade.php
+++ resources/views/web/x-nosotros.blade.php
@@ -10,6 +10,13 @@
 <meta property="og:description" content="Somos una Notaría en Queens New York que ofrece servicios de Apostillas, Poderes, Traducciones, Affidávit, Autorizaciones de Viaje, Cartas de Invitación." />
 <meta property="og:image"       content="{{asset('img/meta-notaria-latina-queens-new-york.jpg')}}" />
 
+<script defer src="{{ asset('js/navbar-style.js') }}"></script>
+
+<style>
+    .navbar-img{
+        filter: brightness(0) invert(1) !important;
+    }
+</style>
 @endsection
 
 @section('phoneNumberHidden', '+18007428602')
@@ -19,18 +26,76 @@
 
 <section id="prisection" style="background-size: cover;background-position: left top; background-repeat: no-repeat;">
     <div>
-
-        <div class="row align-items-center" style="min-height: 550px;background:rgba(2, 2, 2, 0.5)">
+        <div class="row align-items-center" style="min-height: 850px;background:rgba(36, 36, 36, 0.5)">
 
             <div class="col-12 text-white text-center">
-              <h1 class="font-weight-bold heading-title" >Nosotros</h1>
+              <h1 style="font-weight: 400">Descubra nuestra <br> <span style="color: #FFBE32; font-weight: 700">historia y experiencia</span></h1>
           </div>
-
       </div>
     </div>
   </section>
 
-<div class="container pt-4">
+  <section class="row">
+    <article class="col-sm-7 d-flex align-items-center" style="padding-left: 0px !important; padding-right: 0px !important; background-repeat: no-repeat; background-size: cover; background-position: center; background-image: url({{ asset('img/fondo-quienes-somos.png') }});">
+        <div style="padding-left: 25%; padding-right: 10%">
+            <div>
+                <p class="d-flex align-items-center" style="font-size: 14px; color: #122944">NOSOTROS <span class="ml-2" style="background-color: #FFBE32; height: 1.8px; width: 70px"></span></p>
+            </div>
+            <div style="color: #122944;">
+                <h2 style="font-weight: 400">¿Quiénes <span style="font-weight: 700">somos?</span></h2>
+                <div style="font-size: 1.1rem; text-align: justify">
+                    <p>Somos <span style="font-weight: 600">Notaría Pública Latina</span>, nos dedicamos a ofrecer servicios notariales de alta calidad a los 50 estados de EE.UU con el objetivo de brindar la máxima seguridad en la certificación de hechos, de conformidad con las facultades otorgadas por la ley.</p>
+                    <p>Durante más de 15 años, hemos tenido el honor de contar con su confianza, lo que ha contribuidoen gran medida al crecimiento y desarrollo de nuestra empresa.</p>
+                    <p>Nuestra misión principal es proporcionar soluciones integrales a través de un servicio notarial completo y confiable.</p>
+                </div>
+            </div>
+        </div>
+    </article>
+    <article class="col-sm-5" style="padding-left: 0px !important; padding-right: 0px !important">
+        <img class="img-fluid" src="{{ asset('img/notaria-con-sello.png') }}" alt="">
+    </article>
+  </section>
+
+  <section class="row">
+    <article class="col-sm-5 pr-0 pl-0">
+        <img class="img-fluid" src="{{ asset('img/img-nuestra-experiencia.png') }}" alt="">
+    </article>
+    <article class="col-sm-7 pl-0 pr-0 d-flex align-items-center" style="background-color: #122944; color: #ffffff">
+        <div style="padding-left: 10%; padding-right: 25%">
+            <div>
+                <p class="d-flex align-items-center justify-content-end" style="font-size: 14px; color: #ffffff"><span class="mr-2" style="background-color: #FFBE32; height: 1.8px; width: 70px"></span> NOSOTROS</p>
+            </div>
+            <div style="font-size: 1.1rem; text-align: justify; direction: rtl">
+                <h2 style="font-weight: 400">Nuestra <span style="font-weight: 700; color: #FFBE32">experiencia</span></h2>
+                <p>Con una trayectoria de más de 15 años en el campo notarial, en Notaría Pública Latina hemos acumulado una valiosa experiencia que respalda la excelencia en nuestros servicios</p>
+                <p>Nuestra larga historia de compromiso con la comunidad nos ha permitido perfeccionar y agilizar los trámites notariales, convirtiéndonos en un referente de confianza.</p>
+            </div>
+        </div>
+    </article>
+  </section>
+
+  <section class="row">
+    <article class="col-sm-7 d-flex align-items-center" style="padding-left: 0px !important; padding-right: 0px !important; background-repeat: no-repeat; background-size: cover; background-position: center; background-image: url({{ asset('img/fondo-nuestro-personal.png') }});">
+        <div style="padding-left: 25%; padding-right: 10%">
+            <div>
+                <p class="d-flex align-items-center" style="font-size: 14px; color: #122944">NOSOTROS <span class="ml-2" style="background-color: #FFBE32; height: 1.8px; width: 70px"></span></p>
+            </div>
+            <div style="color: #122944;">
+                <h2 style="font-weight: 400">Nuestro <span style="font-weight: 700">personal</span></h2>
+                <div style="font-size: 1.1rem; text-align: justify">
+                    <p>Nuestro equipo está compuesto por profesionales jóvenes y altamente capacitados, comprometidos con brindarle la mejor asistencia en todos los trámites que pueda necesitar</p>
+                    <p>En <span style="font-weight: 600">Notaría Pública Latina</span>, creemos que la juventud, la experiencia y la capacitación se combinan para ofrecer un servicio notarial de primera calidad.</p>
+                    <p>Somos su opción de confianza para todos los asuntos relacionados con la notaría pública, y nos comprometemos a proporcinarle un servicio integral, rápido y eficaz.</p>
+                </div>
+            </div>
+        </div>
+    </article>
+    <article class="col-sm-5" style="padding-left: 0px !important; padding-right: 0px !important">
+        <img class="img-fluid" src="{{ asset('img/img-nuestro-personal.png') }}" alt="">
+    </article>
+  </section>
+
+{{-- <div class="container pt-4">
         <div class="row">
             <div class="col-12 col-md-6 quienes-somos text-center d-flex align-items-center justify-content-center">
                 <img class="py-5 img-fluid" src="{{asset('img/marca-notaria-latina.png')}}" alt="" height="180">
@@ -50,7 +115,7 @@
 
             </div>
         </div>
-</div>
+</div> --}}
 
 
 @endsection
@@ -60,7 +125,7 @@
 @section('script')
 <script>
     window.addEventListener('load', (event) => {
-        document.getElementById('prisection').style.backgroundImage = "url('img/inicio.jpg')";
+        document.getElementById('prisection').style.backgroundImage = "url('img/banner-nosotros.webp')";
     });
   </script>
 @endsection
